98d4f64b76cb9e3df482842485ec206de8c3cfbc
Clementine Urquizar <clementine@meilisearch.com>
2020-08-12 15:45:59 +0200

    Use get_or_create_index of the SDK

--- meilisearch/feed_meilisearch.rb
+++ meilisearch/feed_meilisearch.rb
@@ -1,27 +1,16 @@
 require 'meilisearch'
 
-URL = ENV['MEILISEARCH_URL']
-API_KEY = ENV['MEILISEARCH_MASTER_KEY']
+URL = ENV['MEILISEARCH_HOST_URL']
+API_KEY = ENV['MEILISEARCH_API_KEY']
 
-def get_or_create_index(index_uid, settings = nil)
+def feed_meilisearch_with(documents, index_uid, settings = nil)
   client = MeiliSearch::Client.new(URL, API_KEY)
-  index = client.index(index_uid)
-  begin
-    index.show
-    LOGGER.warn "Index #{index_uid} already exists."
-  rescue MeiliSearch::HTTPError
-    index = client.create_index(index_uid)
-    LOGGER.info "New index #{index_uid} created."
-  end
+  index = client.get_or_create_index(index_uid)
+  LOGGER.info "Index #{index_uid} is ready."
   if settings
     index.update_settings(settings)
     LOGGER.info 'Settings updated.'
   end
-  index
-end
-
-def feed_meilisearch_with(documents, index_uid, settings = nil)
-  index = get_or_create_index(index_uid, settings)
   LOGGER.info 'Adding documents...'
   documents.each_slice(1800).with_index do |slice, i|
     # puts "Adding slice #{i}"
