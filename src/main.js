c49d433a9d83e1502f8bb087ea27b2a648959293
CaroFG <carolina.ferreira131@gmail.com>
2020-12-14 09:39:42 +0100

    Add fontawesome icons and restore vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import InstantSearch from 'vue-instantsearch';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+ 
+library.add(faChevronDown)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(InstantSearch);
 
