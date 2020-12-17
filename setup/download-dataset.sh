4d1a09bfdb31ef19709872cacf3e25ae50079d5e
CaroFG <carolina.ferreira131@gmail.com>
2020-12-17 11:47:29 +0100

    Add condition to script

--- setup/download-dataset.sh
+++ setup/download-dataset.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
 # Download Artworks.json from MoMA's github
-
+if [ -s Artworks ]
+then
 curl -LJO https://github.com/MuseumofModernArt/collection/raw/master/Artworks.json
+else
+echo "File already exists"
+fi
