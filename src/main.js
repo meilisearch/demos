52047ab8be3e57470eb225e96f5d06440c72f88e
CaroFG <carolina.ferreira131@gmail.com>
2020-12-17 11:40:30 +0100

    Fix lint errors

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import InstantSearch from 'vue-instantsearch';
+import InstantSearch from 'vue-instantsearch'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
- 
+
 library.add(faChevronDown, faChevronUp)
- 
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-Vue.use(InstantSearch);
+Vue.use(InstantSearch)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
