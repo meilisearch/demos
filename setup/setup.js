dc2a8f54e139ba93b566abfec8246149dec62e8b
CaroFG <carolina.ferreira131@gmail.com>
2020-12-17 19:38:21 +0100

    Process data before indexing

--- setup/setup.js
+++ setup/setup.js
@@ -9,59 +9,63 @@ require('dotenv').config()
     apiKey: process.env.VUE_APP_MEILISEARCH_API_KEY
   })
 
-  // Create Index
-  await client.createIndex('artWorks', { primaryKey: 'ObjectID' })
-  const index = client.getIndex('artWorks')
-  console.log('Index "artWorks" created.')
+    // Create Index
+    //await client.createIndex('artWorks', { primaryKey: 'ObjectID' })
+    const index = client.getIndex('artWorks')
+    console.log('Index "artWorks" created.');
 
-  // Add settings
-  const settings = {
-    distinctAttribute: null,
-    searchableAttributes: [
-      'Title',
-      'Artist',
-      'ArtistBio',
-      'Nationality',
-      'Gender',
-      'BeginDate',
-      'Date',
-      'Medium',
-      'Department'
-    ],
-    displayedAttributes: [
-      'Title',
-      'Artist',
-      'ArtistBio',
-      'Nationality',
-      'Gender',
-      'BeginDate',
-      'Date',
-      'Medium',
-      'Dimensions',
-      'URL',
-      'Department',
-      'Classification',
-      'ThumbnailURL'
-    ],
-    stopWords: ['a', 'an', 'the'],
-    synonyms: { },
-    attributesForFaceting: [
-      'Nationality', 'Gender', 'Medium', 'Classification'
-    ]
-  }
-  await index.updateSettings(settings)
-  console.log('Settings added to "artWorks" index.')
+    // Add settings
+    const settings = {
+        distinctAttribute: null,
+        searchableAttributes: [
+            'Title',
+            'Artist',
+            'ArtistBio',
+            'Nationality',
+            'Gender',
+            'Date',
+            'Medium',
+            'Department',
+            'MultipleArtists',
+            'DateToSortBy'
+          ],
+        displayedAttributes: [
+            'Title',
+            'Artist',
+            'ArtistBio',
+            'Nationality',
+            'Gender',
+            'Date',
+            'Medium',
+            'Dimensions',
+            'URL',
+            'Department',
+            'Classification',
+            'ThumbnailURL',
+            'MultipleArtists'
+          ],
+        stopWords: ["a", "an", "the"],
+        synonyms: { },
+        attributesForFaceting: [
+            'Nationality', 'Gender', 'Medium', 'Classification'
+        ]
+      }
+    await index.updateSettings(settings)
+    console.log('Settings added to "artWorks" index.');
+    
+    // Process documents
+    const processedDataSet = dataProcessing(dataset)
 
-  // Add documents
-  const batchedDataSet = batch(dataset, 500)
-  console.log('Adding documents...')
-  for (let i = 0; i < batchedDataSet.length; i++) {
-    const { updateId } = await index.addDocuments(batchedDataSet[i])
-    await index.waitForPendingUpdate(updateId, {
-      timeOutMs: 100000
-    })
-  }
-  console.log('Documents added to "artWorks" index.')
+    // Add documents
+    const batchedDataSet = batch(processedDataSet, 500)
+    console.log('Adding documents...');
+    for (let i = 0; i < batchedDataSet.length; i++) {
+        let { updateId } = await index.addDocuments(batchedDataSet[i])
+        await index.waitForPendingUpdate(updateId, {
+            timeOutMs: 100000
+        })
+    }
+    console.log('Documents added to "artWorks" index.')
 })()
 
 // Split dataset into batches
@@ -76,18 +80,41 @@ function batch (array, size) {
 }
 
 // Transform array into string so MeiliSearch can highlight the results
-
 function arrayToString (document) {
   for (const [key, value] of Object.entries(document)) {
-      if (Array.isArray(value)) {
-          let stringValue = value.join(', ')
-          document[key] = stringValue
+    if (key === 'Artist' || key === 'ArtistBio') {
+      let stringValue = value.join(', ')
+      document[key] = stringValue
+      if (key === 'Artist' && value.length > 1) {
+        document.MultipleArtists = true
+      } else {
+        document.MultipleArtists = false
       }
+    } 
   }
   return document
 }
 
+// Get year from Date field and add it to new field to make sorting by date easier
 function normalizeDate (document) {
   const date = document.Date 
   const match = (/(\d{4})/).exec(date) 
+  if (match) {
+    document.DateToSortBy = match[0]
+  } else {
+    document.DateToSortBy = date
+  }
+  return document
 }
+
+// Apply arrayToString and normalizeDate in each document of an array
+function dataProcessing (data) {
+  const processedDataArray = []
+  for (let i = 0; i < data.length; i++ ) {
+    const stringifiedDoc = arrayToString(data[i])
+    const processedDoc = normalizeDate(stringifiedDoc)
+    processedDataArray.push(processedDoc)
+  }
+  return processedDataArray
+}
+
