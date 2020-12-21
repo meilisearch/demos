a48176b29eb497639d8ffa87d15aa256770fe15e
CaroFG <carolina.ferreira131@gmail.com>
2020-12-21 16:21:58 +0100

    Replace single square brackets with double ones

--- setup/download-dataset.sh
+++ setup/download-dataset.sh
@@ -2,7 +2,7 @@
 #
 # Download Artworks.json from MoMA's github
 FILE=Artworks.json
-if [ -f "$FILE" ]; then
+if [[ -f $FILE ]]; then
     echo "$FILE already exists."
 else 
     curl -LJO https://github.com/MuseumofModernArt/collection/raw/master/Artworks.json
