ab88a5bd17063c9f183846a779a558b7ceb3afb5
CaroFG <carolina.ferreira131@gmail.com>
2020-12-29 14:51:24 +0100

    Add icons

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import InstantSearch from 'vue-instantsearch'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faFlag, faVenusMars, faHashtag } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faChevronDown, faChevronUp)
+library.add(faChevronDown, faChevronUp, faFlag, faVenusMars, faHashtag)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(InstantSearch)
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
