2cf5daa87758fa95bacfc60de05a68debd24b185
Samuel Jimenez <sjimenezre@gmail.com>
2021-07-01 02:18:01 +0200

    Update: attributesForFaceting => filterableAttributes for v0.21.0 compatibility

--- setup/launchMeili.js
+++ setup/launchMeili.js
@@ -39,7 +39,7 @@ const settings = {
   ],
   stopWords: ['a', 'an', 'the'],
   synonyms: { },
-  attributesForFaceting: [
+  filterableAttributes: [
     'Nationality', 'Gender', 'Classification'
   ]
 }
