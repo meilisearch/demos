a64f85f7190f9cdacd120057ff321cde28d33a74
shivay <shivaylamba@gmail.com>
2022-05-25 20:43:03 +0530

    improve messages for data seed

--- src/ecommerce/seed/seed.js
+++ src/ecommerce/seed/seed.js
@@ -1,36 +1,34 @@
-require('dotenv').config();
-const { watchUpdates } = require('./utility');
-const fs = require('fs');
-const { MeiliSearch } = require('meilisearch');
+require("dotenv").config();
+const { watchUpdates } = require("./utility");
+const fs = require("fs");
+const { MeiliSearch } = require("meilisearch");
 
 const client = new MeiliSearch({
   host: process.env.NEXT_PUBLIC_MEILI_HOST_NAME,
   apiKey: process.env.NEXT_PUBLIC_MEILI_API_KEY,
 });
 
-const INDEX_NAME = 'products';
+const INDEX_NAME = "products";
 
 const index = client.index(INDEX_NAME);
 
-const data = require('./data.json');
+const data = require("./data.json");
 
 (async () => {
+  console.log(`Adding Filterable and Sortable Attributes to "${INDEX_NAME}"`);
   await index.updateFilterableAttributes([
-    'brand',
-    'category',
-    'tag',
-    'rating',
-    'reviews_count',
-    'price',
+    "brand",
+    "category",
+    "tag",
+    "rating",
+    "reviews_count",
+    "price",
   ]);
-  console.log('Filterable Attributes Updated');
+  await index.updateSortableAttributes(["reviews_count", "rating", "price"]);
 
-  await index.updateSortableAttributes(['reviews_count', 'rating', 'price']);
-  console.log('Sortable Attributes Updated');
-
+  console.log(`Adding Documents to "${INDEX_NAME}"`);
   await index.updateDocuments(data);
-  console.log('Documents Updated');
+  console.log("=============");
 
   await watchUpdates(client, INDEX_NAME);
 })();
-
--- src/ecommerce/seed/utility.js
+++ src/ecommerce/seed/utility.js
@@ -6,18 +6,18 @@ exports.watchUpdates = async (client, uid) => {
   const standardSpeed = 500;
   let allProcessed = false;
   console.log(`Start update watch for ${uid}`);
-  console.log('-------------');
+  console.log("-------------");
   while (!allProcessed) {
     try {
       const updates = await client.index(uid).getTasks();
       const processed = updates.results.filter(
-        (update) => update.status === 'succeeded'
+        (update) => update.status === "succeeded"
       );
       const processing = updates.results.filter(
-        (update) => update.status === 'processing'
+        (update) => update.status === "processing"
       );
       const enqueued = updates.results.filter(
-        (update) => update.status === 'enqueued'
+        (update) => update.status === "enqueued"
       );
       console.log(`${uid}:`);
       console.log(
@@ -27,12 +27,12 @@ exports.watchUpdates = async (client, uid) => {
         `${processing.length} / ${updates.results.length} is being processed`
       );
       console.log(`${enqueued.length} / ${updates.results.length} still enqueued`);
-      console.log('-------------');
+      console.log("-------------");
       if (enqueued.length === 0) allProcessed = true;
       await sleep(standardSpeed);
     } catch (e) {
       console.error(e);
     }
   }
-  console.log(`All documents added to "${uid}"`);
+  console.log(`All documents and attributes are added to "${uid}"`);
 };
