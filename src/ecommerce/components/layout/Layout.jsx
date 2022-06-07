12e44f28f5d5fef9fa1aea104e4c2ada5432f9c4
shivay <shivaylamba@gmail.com>
2022-06-07 12:57:43 -0500

    remove blur component

--- src/ecommerce/components/layout/Layout.jsx
+++ src/ecommerce/components/layout/Layout.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import NavBar from './NavBar'
-import Blur from './Blur'
-import { InstantSearch } from 'react-instantsearch-dom'
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+import React from 'react';
+import NavBar from './NavBar';
+import { InstantSearch } from 'react-instantsearch-dom';
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 
 const searchClient = instantMeiliSearch(
   process.env.NEXT_PUBLIC_MEILI_HOST_NAME,
   process.env.NEXT_PUBLIC_MEILI_API_KEY
-)
+);
 
 const Layout = ({ children }) => {
   return (
-    <InstantSearch indexName='products' searchClient={searchClient}>
-      <Blur />
+    <InstantSearch indexName="products" searchClient={searchClient}>
       <NavBar />
       <main>{children}</main>
     </InstantSearch>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
