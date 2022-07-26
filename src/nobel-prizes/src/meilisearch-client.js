2c895af3ea6c6bd01cf1bc6de54eed85c79c3006
Oluwaseyi Ademola <101267639+oluademola@users.noreply.github.com>
2022-07-26 10:29:43 +0100

    replace the search apiKey with a deterministic key

--- src/nobel-prizes/src/meilisearch-client.js
+++ src/nobel-prizes/src/meilisearch-client.js
@@ -1,5 +1,5 @@
 const MEILISEARCH_HOST = "https://ms-a79a247a550f-106.lon.meilisearch.io"
-const MEILISEARCH_API_KEY = "7cc165966f5abc529c81e8945f5d6134c5cae76fb9b2f1fc3e7134683bf86078"
+const MEILISEARCH_API_KEY = "87b5f3f9fd344ccad333adce78f21309fdecb4a72fa48608df1e9c6935d8c647"
 
 export {
   MEILISEARCH_HOST,
