381de361649d329d9689d01bd911a645341cdca5
CaroFG <carolina.ferreira131@gmail.com>
2022-09-07 09:39:34 +0200

    Add fathom script to ecommerce demo

--- src/ecommerce/pages/_app.jsx
+++ src/ecommerce/pages/_app.jsx
@@ -2,8 +2,31 @@ import Head from 'next/head'
 import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 import '../styles/searchBoxAIS.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('QNBPJXIV', {
+      includedDomains: ['meilisearch.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <Head>
