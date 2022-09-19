68b38f5cb73a85b8d87f7b322776b81fe3fcbe8f
CaroFG <carolina.ferreira131@gmail.com>
2022-09-19 16:16:02 +0200

    Add demo

---
+++ src/matching-strategy/setup/setupFunctions.js
@@ -0,0 +1,34 @@
+async function populateIndex (uid, dataset, client) {
+  return await client.index(uid).addDocuments(dataset)
+}
+
+async function sleep (ms) {
+  return await new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function watchTasks (client, uid) {
+  const standardSpeed = 500
+  let allProcessed = false
+  console.log(`Start update watch for ${uid}`)
+  console.log('-------------')
+  while (!allProcessed) {
+    try {
+      const tasks = await client.index(uid).getTasks()
+      const enqueued = tasks.results.filter(update => update.status === 'enqueued')
+      console.log(`${uid}:`)
+      console.log('Adding documents')
+      console.log('-------------')
+      if (enqueued.length === 0) allProcessed = true
+      await sleep(standardSpeed)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  console.log(`All documents added to "${uid}"`)
+}
+
+export {
+  populateIndex,
+  sleep,
+  watchTasks
+}
