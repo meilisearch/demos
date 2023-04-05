06103c164c611d0d146c7c8a68b873f094661cc1
CaroFG <carolina.ferreira131@gmail.com>
2023-04-05 17:09:54 +0200

    Add meta title and fathom script

--- src/federated-search/pages/_document.js
+++ src/federated-search/pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css"
         />
+        <title>Meilisearch demo: federated search</title>
+        <script src="https://thrilling-thirtyeight.meilisearch.com/script.js" data-site="QNBPJXIV" defer></script>
       </Head>
       <body>
         <Main />
