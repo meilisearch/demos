9a9d354b344e7d86f92c079d1e74311225860489
CaroFG <carolina.ferreira131@gmail.com>
2023-04-06 23:22:28 +0200

    Remove transformItems function

--- src/federated-search/components/Results/ActorHits.js
+++ src/federated-search/components/Results/ActorHits.js
@@ -3,27 +3,11 @@ import ActorCard from './ActorCard'
 import { InfiniteHits } from 'react-instantsearch-hooks-web'
 
 const ActorHits = () => {
-  const transformActorsItems = items => {
-    return items.map(item => ({
-      ...item,
-      _highlightResult: {
-        ...item._highlightResult,
-        known_for: item?._highlightResult?.known_for?.reduce((acc, curr) => {
-          if (curr?.original_title) {
-            acc.push(curr.original_title)
-            return acc
-          }
-          return acc
-        }, []),
-      },
-    }))
-  }
   return (
     <div className="resultsContainer">
       <InfiniteHits
         hitComponent={ActorCard}
         showPrevious={false}
-        transformItems={transformActorsItems}
         classNames={{
           list: 'resultsList',
           item: 'card',
