f7b041914a22d26a89ba3b510653d087ca0e1445
CaroFG <carolina.ferreira131@gmail.com>
2025-06-23 22:42:11 +0200

    Fix lint error

--- src/federated-search/pages/_document.js
+++ src/federated-search/pages/_document.js
@@ -5,13 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google Tag Manager */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TNSCGVBH');`
+            })(window,document,'script','dataLayer','GTM-TNSCGVBH');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -23,14 +24,14 @@ export default function Document() {
           src="https://thrilling-thirtyeight.meilisearch.com/script.js"
           data-site="QNBPJXIV"
           defer
-        ></script>
+        />
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNSCGVBH"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
         {/* End Google Tag Manager (noscript) */}
