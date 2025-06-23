389a53e50042a410154d2ec620b45ae41b86bbc9
CaroFG <carolina.ferreira131@gmail.com>
2025-06-23 22:17:17 +0200

    Add GTM to federated search demo

--- src/federated-search/pages/_document.js
+++ src/federated-search/pages/_document.js
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TNSCGVBH');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css"
@@ -15,6 +26,14 @@ export default function Document() {
         ></script>
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNSCGVBH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
