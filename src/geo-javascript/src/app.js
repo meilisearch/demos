a113adf71a7480e91b84b3a5b9fb2c9fb9d3cbda
CaroFG <carolina.ferreira131@gmail.com>
2026-03-18 15:23:25 +0100

    Update UI

--- src/geo-javascript/src/app.js
+++ src/geo-javascript/src/app.js
@@ -5,13 +5,13 @@ import { setOptions, importLibrary } from '@googlemaps/js-api-loader'
 const GOOGLE_MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 setOptions({
-  apiKey: GOOGLE_MAP_API_KEY,
-  version: 'weekly',
+  key: GOOGLE_MAP_API_KEY,
+  v: 'weekly',
 })
 
 importLibrary('maps').then(() => {
   const search = instantsearch({
-    indexName: 'world_cities_geojson',
+    indexName: 'world_cities_geojson:population:desc',
     searchClient: instantMeiliSearch(
       'https://ms-adf78ae33284-106.lon.meilisearch.io',
       'a63da4928426f12639e19d62886f621130f3fa9ff3c7534c5d179f0f51c4f303',
@@ -49,23 +49,28 @@ importLibrary('maps').then(() => {
         lng: 4.843585698860502,
       },
       enableRefineOnMapMove: false,
-      enableClearMapRefinement: false,
+      enableClearMapRefinement: true,
       enableRefineControl: false,
     }),
     instantsearch.widgets.infiniteHits({
       container: '#hits',
+      transformItems: (items) =>
+        items.map((item) => ({
+          ...item,
+          populationFormatted: item.population
+            ? new Intl.NumberFormat().format(item.population)
+            : 'N/A',
+        })),
       templates: {
         item: `
-          <div>
-            <div class="hit-name">
-              City: {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+          <div class="hit-card">
+            <div class="hit-city-name">
+              {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
             </div>
-            <div class="hit-name">
-              Country: {{#helpers.highlight}}{ "attribute": "country" }{{/helpers.highlight}}
+            <div class="hit-country">
+              {{#helpers.highlight}}{ "attribute": "country" }{{/helpers.highlight}}
             </div>
-            <div class="hit-name">
-              Population: {{#helpers.highlight}}{ "attribute": "population" }{{/helpers.highlight}}
-            </div>
+            <div class="hit-population">{{populationFormatted}} pop.</div>
           </div>
         `,
       },
